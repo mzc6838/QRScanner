7792a5d64fbe63dbd765b3cd25fd70c67616992f
Janzen Ma <383287419@qq.com>
2019-09-13 13:06:37 +0800

    斗图没了

--- app/src/main/java/xyz/mzc6838/qrscanner/activity/MainActivity.java
+++ app/src/main/java/xyz/mzc6838/qrscanner/activity/MainActivity.java
@@ -1,55 +1,29 @@
-package xyz.mzc6838.qrscanner;
+package xyz.mzc6838.qrscanner.activity;
 
 import android.Manifest;
-import android.app.Activity;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.content.ClipData;
-import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.media.projection.MediaProjectionManager;
 import android.os.Build;
-import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.util.DisplayMetrics;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
-import com.baidu.ocr.sdk.OCR;
-import com.baidu.ocr.sdk.OnResultListener;
-import com.baidu.ocr.sdk.exception.OCRError;
-import com.baidu.ocr.sdk.model.AccessToken;
-import com.baidu.ocr.sdk.model.GeneralBasicParams;
-import com.baidu.ocr.sdk.model.GeneralResult;
-import com.baidu.ocr.sdk.model.WordSimple;
-import com.google.gson.Gson;
-import com.google.zxing.BinaryBitmap;
-import com.google.zxing.ChecksumException;
-import com.google.zxing.DecodeHintType;
-import com.google.zxing.FormatException;
-import com.google.zxing.NotFoundException;
-import com.google.zxing.RGBLuminanceSource;
-import com.google.zxing.Result;
-import com.google.zxing.client.android.CaptureActivity;
-import com.google.zxing.common.HybridBinarizer;
-import com.google.zxing.qrcode.QRCodeReader;
-
-import java.io.File;
-import java.util.EnumMap;
-import java.util.Map;
+import xyz.mzc6838.qrscanner.R;
+import xyz.mzc6838.qrscanner.service.ColorPickService;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -58,6 +32,11 @@ public class MainActivity extends AppCompatActivity {
     Button fightImg;
     Button createQRCode;
     Button colorPicker;
+
+    MediaProjectionManager mMediaProjectionManager;
+    //static MediaProjection mMediaProjection;
+
+    SurfaceView mSurfaceView;
 
 
     @Override
@@ -74,6 +53,8 @@ public class MainActivity extends AppCompatActivity {
         fightImg = findViewById(R.id.fightImgButton);
         createQRCode = findViewById(R.id.createQRCode);
         colorPicker = findViewById(R.id.colorPickerButton);
+
+        //mSurfaceView = findViewById(R.id.mSurface);
 
         scan.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -97,8 +78,9 @@ public class MainActivity extends AppCompatActivity {
         fightImg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, FightImgActivity.class);
-                startActivity(intent);
+                Toast.makeText(MainActivity.this, "斗图功能已停用！", Toast.LENGTH_SHORT).show();
+//                Intent intent = new Intent(MainActivity.this, FightImgActivity.class);
+//                startActivity(intent);
             }
         });
 
@@ -108,15 +90,12 @@ public class MainActivity extends AppCompatActivity {
         });
 
         colorPicker.setOnClickListener((v)->{
-            //TODO 取色器service创建 与 通知的创建
-            Intent intent = new Intent(MainActivity.this, ColorPickService.class);
-            startService(intent);
 
             Notification notification;
             NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
             if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-                NotificationChannel notificationChannel = new NotificationChannel("mzc6838", "close_color_picker", NotificationManager.IMPORTANCE_HIGH);
+                NotificationChannel notificationChannel = new NotificationChannel("mzc6838", "close_color_picker", NotificationManager.IMPORTANCE_DEFAULT);
                 notificationChannel.enableLights(false);
                 notificationChannel.setShowBadge(true);
                 if(notificationManager != null)
@@ -127,19 +106,22 @@ public class MainActivity extends AppCompatActivity {
             PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 1, intent1, PendingIntent.FLAG_UPDATE_CURRENT);
 
             if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-            notification = new Notification.Builder(this, "mzc6838")
-                    .setContentTitle("QRScanner")
-                    .setContentText("点击这里关闭取色器")
-                    .setWhen(System.currentTimeMillis())
-                    .setAutoCancel(true)
-                    .setOngoing(true)
-                    .setContentIntent(pendingIntent)
-                    .build();
+                notification = new Notification.Builder(this, "mzc6838")
+                        .setContentTitle("QRScanner")
+                        .setContentText("点击这里关闭取色器")
+                        .setWhen(System.currentTimeMillis())
+                        .setSmallIcon(R.mipmap.ic_launcher_my_round)
+                        .setAutoCancel(true)
+                        .setOngoing(true)
+                        .setSmallIcon(R.mipmap.ic_launcher_my_round)
+                        .setContentIntent(pendingIntent)
+                        .build();
             }else{
                 notification = new NotificationCompat.Builder(this)
                         .setContentTitle("QRScanner")
                         .setContentText("点击这里关闭取色器")
                         .setWhen(System.currentTimeMillis())
+                        .setSmallIcon(R.mipmap.ic_launcher_my_round)
                         .setAutoCancel(true)
                         .setOngoing(true)
                         .setContentIntent(pendingIntent)
@@ -148,6 +130,9 @@ public class MainActivity extends AppCompatActivity {
 
             if(notificationManager != null)
                 notificationManager.notify(255, notification);
+
+            mMediaProjectionManager = (MediaProjectionManager) getSystemService(MEDIA_PROJECTION_SERVICE);
+            startActivityForResult(mMediaProjectionManager.createScreenCaptureIntent(), 5);
         });
 
 
@@ -161,6 +146,28 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, final int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+
+        switch(requestCode){
+            case (5):{
+                if(resultCode == RESULT_OK){
+                    //mMediaProjection = mMediaProjectionManager.getMediaProjection(requestCode, data);
+                    Intent intent = new Intent(MainActivity.this, ColorPickService.class);
+
+                    DisplayMetrics displayMetrics = new DisplayMetrics();
+                    getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+
+                    intent.putExtra("width", displayMetrics.widthPixels);
+                    intent.putExtra("height", displayMetrics.heightPixels);
+                    intent.putExtra("dpi", displayMetrics.densityDpi);
+
+                    intent.putExtra("resultCode", resultCode);
+                    intent.putExtra("data", data);
+
+                    this.startService(intent);
+                }
+                break;
+            }
+        }
     }
 
     @Override
@@ -176,5 +183,4 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
-
 }
